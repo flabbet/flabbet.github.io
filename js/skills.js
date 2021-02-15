091fc514489f0646351719f3b517653d4e17aea8
flabbet <flubbet@gmail.com>
2021-02-15 21:12:33 +0100

    Added Git to skills

--- js/skills.js
+++ js/skills.js
@@ -1,5 +1,5 @@
 const skills = {
     languages: ["C#", "JavaScript", "Python", "Rust", "Dart", "Kotlin", "CSS", "HTML", "XAML"],
     technologies: ["WPF", "Unity", "AvaloniaUI", "ASP.NET Core", "React", "Android", "Flutter", "Flask", "SQLite", "ArangoDB", "Arduino"],
-    other: ["GitHub", "Azure", "Azure DevOps", "Linux", "Docker"]
+    other: ["Git", "GitHub", "Azure", "Azure DevOps", "Linux", "Docker"]
 }

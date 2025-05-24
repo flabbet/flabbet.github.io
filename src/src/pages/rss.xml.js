cd0c7e06c1f5ab0fe32e0d0b9799743061e9ed6b
Krzysztof Krysiński <flubbet@gmail.com>
2025-05-24 14:53:28 +0200

    Update rss.xml.js

--- src/src/pages/rss.xml.js
+++ src/src/pages/rss.xml.js
@@ -6,6 +6,12 @@ export async function GET(context) {
     description: 'I can make it better - a blog about remaking those little and (way too) big things in programming world.',
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-    customData: `<language>en</language>`,
+    customData: `
+    <language>en</language>
+    <follow_challenge>
+    <feedId>149083849102513152</feedId>
+    <userId>149083075016414208</userId>
+    </follow_challenge>
+    `,
   });
 }
